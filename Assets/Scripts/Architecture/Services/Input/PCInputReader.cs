d9863d756a3900e01ae4b43bf46d647cb120d3ca
qdiaps <qdiaps@gmail.com>
2024-08-20 14:05:13 +0300

    Create Input Reader (PC)

---
+++ Assets/Scripts/Architecture/Services/Input/PCInputReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using VContainer.Unity;
+
+namespace Architecture.Services.Input
+{
+    public class PCInputReader : IInputReader, ITickable
+    {
+        public event Action<Vector3> OnMove;
+        public event Action<Vector3> OnSprintMove;
+        public event Action OnJump;
+        
+        private const string AxisHorizontal = "Horizontal";
+        private const string AxisVertical = "Vertical";
+
+        public void Tick()
+        {
+            ReadMove();
+            ReadJump();
+        }
+
+        private void ReadMove()
+        {
+            var velocity = new Vector3(UnityEngine.Input.GetAxis(AxisHorizontal), 0, UnityEngine.Input.GetAxis(AxisVertical));
+            if (velocity.x != 0 || velocity.z != 0)
+            {
+                if (UnityEngine.Input.GetKey(KeyCode.LeftShift))
+                {
+                    OnSprintMove?.Invoke(velocity);
+                    return;
+                }
+
+                OnMove?.Invoke(velocity);
+            }
+        }
+
+        private void ReadJump()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+                OnJump?.Invoke();
+        }
+    }
+}
