07c27a91584fed66039de428c519fd2ca76cb79f
qdiaps <qdiaps@gmail.com>
2024-09-06 05:19:21 +0300

    Delete scanner model

--- Assets/Scripts/Configs/Scanner/ScannerConfig.cs
+++ Assets/Scripts/Configs/Scanner/ScannerConfig.cs
@@ -5,13 +5,9 @@ namespace Configs.Scanner
     [CreateAssetMenu(fileName="NewScannerConfig", menuName="CustomConfigs/ScannerConfig")]
     public class ScannerConfig : ScriptableObject
     {
-        [Header("Scan settings")]
         public int PointsPerScan;
         public float Radius;
         public float MaxDistance;
         public float TimeSpawn;
-        [Header("Model params")]
-        public int AttemptRate;
-        public float AllAttemptResetTime;
     }
 }
