b128a850d68145530ab44be03a45273b82039400
qdiaps <qdiaps@gmail.com>
2024-09-08 11:53:06 +0300

    Create finite state machine

---
+++ Assets/Scripts/Architecture/FiniteStateMachine/Fsm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Architecture.FiniteStateMachine.States;
+using UnityEngine;
+
+namespace Architecture.FiniteStateMachine
+{
+    public class Fsm
+    {
+        private State _currentState;
+        private Dictionary<Type, State> _states;
+
+        public void AddState(State state)
+        {
+            if (_states.ContainsKey(state.GetType()))
+            {
+                Debug.LogWarning("State already added");
+                return;
+            }
+            _states.Add(state.GetType(), state);
+        }
+
+        public Type GetState() => 
+            _currentState?.GetType();
+
+        public void SetState(Type typeState)
+        {
+            if (_currentState.GetType() == typeState)
+            {
+                Debug.LogWarning("typeState already set");
+                return;
+            }
+            else if (_states.ContainsKey(typeState) == false)
+            {
+                Debug.LogWarning("typeState not added");
+                return;
+            }
+            _currentState?.Exit();
+            _currentState = _states[typeState];
+            _currentState.Enter();
+        }
+    }
+}
