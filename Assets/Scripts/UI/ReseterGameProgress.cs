4fbbc1844f32bd0d486bf41ec360ada69e31ed49
qdiaps <qdiaps@gmail.com>
2024-09-29 14:08:08 +0300

    Add reset progress

---
+++ Assets/Scripts/UI/ReseterGameProgress.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Architecture.Model.Level;
+using UnityEngine;
+
+namespace UI
+{
+    public class ReseterGameProgress : MonoBehaviour
+    {
+        public void ResetProgress()
+        {
+            var path = CreatePath(typeof(LevelData).ToString());
+            if (File.Exists(path))
+                File.Delete(path);
+        }
+        
+        private string CreatePath(string name) =>
+            Path.Combine(Application.persistentDataPath, $"{name}.json");
+    }
+}
